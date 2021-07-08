a967af3440ebe13a0caad0eab521840925cbf9c0
oosaruyi <oosaruyi@gmail.com>
2021-07-08 18:20:07 +0000

    calorie counter calculator

--- calc.py
+++ calc.py
@@ -1,12 +1,37 @@
-#greet the user
-print("Hello, I'm a fortune teller")
-#ask the user if they want their fortune told
-answer = input("Do you want a fortune? ")
 
-while answer.lower() != "no":
-    print("User asked for a fortune")
-#fortune given 
+totalCal = int(0)
+calBurnt = int(0)
 
-#ask if user wants another fortune
+qA = input("Would you like to input calories? y/n ")
+## Calculates Calories Consumed
+while qA.lower() != "n":
+    meal = int(input("Input calories of meal: "))
+    totalCal = totalCal + meal
+    qA = input("Would you like to continue inputting calories? y/n ")
+print(str(totalCal) + " calories consumed.")
 
-#repeat until user quits
+weight = int(input("Enter Weight (in Pounds): "))
+height = int(input("Enter Height: (in inches): "))
+age = int(input("Enter Age: "))
+gender = input("Enter gender: m/f: ")
+
+def caloriesBurned(w , h , a ,g):
+    if g.lower() == "m":
+       calBurnt = 66 + (6.2*w) + (2.7*h) - (6.76*a)
+    elif g.lower() == "f":
+        calBurnt = 655.1 + (4.35*w) + (4.7*h) - (4.7*a)
+    else:
+        return "Error: incorrect gender input"
+    return calBurnt
+
+print(caloriesBurned(weight,height,age,gender))
+
+def calDeficit(calBurnt):
+    if (totalCal > calBurnt):
+        return(f"You consumed {totalCal - calBurnt} excess Calories.")
+    elif totalCal < calBurnt:
+        return(f"You are in a caloric deficit by {calBurnt - totalCal} calories")
+    else:
+        return "You have reached your daily caloric count"
+print(caloriesBurned(weight, height, age, gender))
+print(calDeficit(caloriesBurned(weight, height, age, gender)))
---
+++ test_planner.py
@@ -0,0 +1,17 @@
+import planner
+
+# Code out your tests here. Some sample tests for the first function have already been written.
+# Remember to use the dot syntax of "planner.functionName()" when referencing functions from the other file.
+
+#print(planner.perfect_day('sunny'))
+
+#print(planner.perfect_day('rainy'))
+
+#print(planner.outdoor_fun('windy'))
+
+print(planner.temp_check('sunny',60))
+
+
+
+## ^ The tests for the first function `perfect_day` have been written for you, but you'll need to write the rest yourself as you go. 
+##   REMEMBER: You need to write one test for each desired outcome, so if there are 5 possible results, you'll need at least five tests to prove that it works. 
